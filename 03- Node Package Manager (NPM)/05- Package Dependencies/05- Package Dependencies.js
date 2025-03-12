8f42879cd5a16eeb8091353549e9bf2d4a7fd689
Reacher <macryze@icloud.com>
2025-03-12 18:19:03 +0800

    Add mongoose dependency and explain node_modules structure
    This commit introduces a `package.json` with mongoose as a dependency, along with a generated `package-lock.json` file. Additionally, a script highlights the structure and characteristics of the `node_modules` directory for better understanding.

---
+++ 03- Node Package Manager (NPM)/05- Package Dependencies/05- Package Dependencies.js
@@ -0,0 +1,17 @@
+// ===========================================================
+//                      Mongoose Package Import
+// ===========================================================
+
+// Load mongoose package for MongoDB interaction
+const mongoose = require("mongoose");
+
+// ===========================================================
+//                Understanding node_modules Structure
+// ===========================================================
+
+// "node_modules" automatically generated upon installing a package like mongoose.
+// Other packages are dependent upon mongoose.
+
+// Characteristics of flat structure in node_modules:
+// 1. Modules are not nested deeply, easier to manage.
+// 2. Reusable modules help avoid duplicates and save space.
