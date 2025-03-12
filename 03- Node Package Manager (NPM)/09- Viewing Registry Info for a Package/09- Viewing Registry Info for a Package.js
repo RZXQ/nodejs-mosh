b06f19224ea99c70ebaece3a067b882d31b03314
Reacher <macryze@icloud.com>
2025-03-12 19:58:38 +0800

    Add example script for using `npm view` command
    This script outlines basic usage of the `npm view` command to retrieve package details, dependencies, and available versions from the npm registry. It includes clear examples for practical reference.

---
+++ 03- Node Package Manager (NPM)/09- Viewing Registry Info for a Package/09- Viewing Registry Info for a Package.js
@@ -0,0 +1,22 @@
+// ===========================================
+//                  npm view
+// ===========================================
+
+// Displays detailed information about a package available on npm
+
+// Usage examples:
+
+// 1. General details about a package:
+// npm view <package-name>
+// Example:
+// npm view mongoose
+
+// 2. Display dependencies of a specific package:
+// npm view <package-name> dependencies
+// Example:
+// npm view mongoose dependencies
+
+// 3. List all available versions of a specific package:
+// npm view <package-name> versions
+// Example:
+// npm view mongoose versions
