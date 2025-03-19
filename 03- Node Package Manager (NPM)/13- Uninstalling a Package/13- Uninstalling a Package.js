8322066f01f2eef13282ca8da4702eaeff533b60
Reacher <macryze@icloud.com>
2025-03-19 20:00:08 +0800

    Add example for uninstalling a package using NPM
    Provide a code example and explanation for uninstalling the `mongoose` package using `npm uninstall`. Clarifies its effect on `node_modules` and `package.json` dependencies.

---
+++ 03- Node Package Manager (NPM)/13- Uninstalling a Package/13- Uninstalling a Package.js
@@ -0,0 +1,6 @@
+// To uninstall the mongoose package, run:
+// npm uninstall mongoose
+
+// This command will:
+// 1. Remove mongoose from the node_modules folder
+// 2. Remove mongoose from the dependencies listed in package.json
