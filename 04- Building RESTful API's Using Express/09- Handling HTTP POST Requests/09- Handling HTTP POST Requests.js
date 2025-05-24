f6066280292c56b0f741123115a6811958316bfc
Reacher <macryze@icloud.com>
2025-05-24 14:19:10 +0800

    Refactor comments and add inspection for HTTP headers
    Simplified comments to improve readability in the Express POST request example. Updated inspection settings to include a check for HTTP header correctness with configurable custom headers.

--- 04- Building RESTful API's Using Express/09- Handling HTTP POST Requests/09- Handling HTTP POST Requests.js
+++ 04- Building RESTful API's Using Express/09- Handling HTTP POST Requests/09- Handling HTTP POST Requests.js
@@ -1,34 +1,30 @@
 const express = require("express");
 const app = express();
 
-// express.json() is a built-in middleware function provided by Express.
-// It parses incoming requests with JSON payloads.
-// When used, request body data becomes available in req.body
-// (automatically parses stringified JSON into JavaScript objects).
+// Enable JSON parsing middleware for request bodies
 app.use(express.json());
 
-// Define an initial array of course objects for example data storage.
 const courses = [
   { id: 1, name: "course1" },
   { id: 2, name: "course2" },
   { id: 3, name: "course3" },
 ];
 
-// HTTP POST request endpoint to add a new course to the server.
-// URL: "/api/courses"
-// Usage: Client sends course object in request body as JSON.
+// POST http://localhost:3000/api/courses
+// Content-Type: application/json
+//
+// {
+//   "name":"reactjs"
+// }
+app.get("/api/courses", (req, res) => res.send(courses));
 app.post("/api/courses", (req, res) => {
-  // Construct a new course object using:
-  // 1. An id which uniquely identifies the new course (simple auto-increment based on existing array length).
-  // 2. A 'name' property, obtained from the request's body.
-  const course = { id: courses.length + 1, name: req.body.name };
-
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
   courses.push(course);
-
-  // Return the newly added course object back to the client, indicating a successful creation.
   res.send(course);
 });
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening: ${port}`));
